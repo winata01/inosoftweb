b0f0343001632e344224e6980faa18349a135260
Winata <winata01@gmail.com>
2026-04-14 18:56:32 +0700

    vite config

--- vite.config.ts
+++ vite.config.ts
@@ -23,7 +23,7 @@ export default defineConfig(({ mode }) => {
         input: {
           main: path.resolve(__dirname, 'index.html'),
           about: path.resolve(__dirname, 'about.html'),
-          services: path.resolve(__dirname, 'services.html'),
+          engagement: path.resolve(__dirname, 'engagement.html'),
           privacy: path.resolve(__dirname, 'privacy.html'),
         },
       },
