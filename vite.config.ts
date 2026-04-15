cb22bdfb0590a9745578ca1900ed25a4e98b2a0a
Winata <winata01@gmail.com>
2026-04-15 17:43:54 +0700

    v.1.1

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
   return {
     base: '/',
+    appType: 'mpa',
     plugins: [tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
@@ -23,6 +24,7 @@ export default defineConfig(({ mode }) => {
         input: {
           main: path.resolve(__dirname, 'index.html'),
           about: path.resolve(__dirname, 'about.html'),
+          services: path.resolve(__dirname, 'services.html'),
           engagement: path.resolve(__dirname, 'engagement.html'),
           privacy: path.resolve(__dirname, 'privacy.html'),
         },
